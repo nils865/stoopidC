840968df4493891cef740678d2a69208f107025e
nils865 <nils.staas0806@gmail.com>
2023-03-03 10:48:28 +0100

    line reading from files

--- src/utils/fileHandling/fileHandling.c
+++ src/utils/fileHandling/fileHandling.c
@@ -2,14 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEFAULT_SIZE 5
+
 char **loadFile(char *filename)
 {
+    // load file
     FILE *file = fopen(filename, "r");
 
     if (file == NULL)
     {
         printf("Could not open file %s\n", filename);
         exit(1);
+    }
+
+    // read file
+    char ch;
+    size_t lineSize = DEFAULT_SIZE;
+    char *currentLine = (char*)malloc(lineSize * sizeof(char));
+    currentLine[0] = '\0';
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        if (ch == '\n')
+        {
+            printf("%s\n", currentLine);
+            currentLine = (char*)malloc(lineSize * sizeof(char));
+            currentLine[0] = '\0';
+            lineSize = DEFAULT_SIZE;
+            continue;
+        }
+
+        if (strlen(currentLine) + 1 >= lineSize)
+            currentLine = (char*)realloc(currentLine, (lineSize *= 2) * sizeof(char));
+
+        char str[] = { ch , '\0' };
+
+        strcat(currentLine, str);
     }
 
     return NULL;
