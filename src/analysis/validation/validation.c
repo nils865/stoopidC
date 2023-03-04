6f38c749cf18160c3fc1313b07bbe0814d23ce69
nils865 <nils.staas0806@gmail.com>
2023-03-04 15:48:12 +0100

    word type setter

--- src/analysis/validation/validation.c
+++ src/analysis/validation/validation.c
@@ -1,3 +1,5 @@
+#include "../lexer/lexer.h"
+
 const char *keywords[] = {
     "var",
     "arr",
@@ -37,3 +39,18 @@ const char *punctuations[] = {
 const char *comments[] = {
     "#"
 };
+
+const char *wordTypes[] = {
+    "Keyword",
+    "Identifier",
+    "Literal",
+    "Operator",
+    "Punctuation",
+    "Comment"
+};
+
+void setWordType(Word *word)
+{
+    char *value = word->value;
+
+}
