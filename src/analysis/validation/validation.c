f8acf3edd15f30ab98c8a0de4ba8d5b177cb434f
nils865 <nils.staas0806@gmail.com>
2023-03-04 15:43:36 +0100

    all special words as validation list

---
+++ src/analysis/validation/validation.c
@@ -0,0 +1,39 @@
+const char *keywords[] = {
+    "var",
+    "arr",
+    "out",
+    "goto",
+    "sleep",
+    "goif",
+    "if",
+    "end",
+    "exit"
+};
+
+const char *operators[] = {
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    "^",
+    "==",
+    "!=",
+    ">",
+    "<",
+    ">=",
+    "<=",
+    "and",
+    "or",
+    "not"
+};
+
+const char *punctuations[] = {
+    ":",
+    "(",
+    ")"
+};
+
+const char *comments[] = {
+    "#"
+};
