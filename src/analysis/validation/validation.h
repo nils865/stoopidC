9bca59becb825b178ce4285f1ee9997ee11723bf
nils865 <nils.staas0806@gmail.com>
2023-03-05 19:56:07 +0100

    made validation lists public

--- src/analysis/validation/validation.h
+++ src/analysis/validation/validation.h
@@ -3,6 +3,12 @@
 
 #include "../wordComponent/wordComponent.h"
 
+extern char *keywords[];
+extern char *operators[];
+extern char *punctuations[];
+extern char *comments[];
+extern char *wordTypes[];
+
 char *getWordType(char *value);
 
 Word validateComment(Word commentPrefix, char *comment);
