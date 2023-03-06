833af837c90aeb76b92f665bcbb90dd3098cc707
nils865 <nils.staas0806@gmail.com>
2023-03-06 14:32:14 +0100

    ansi color code utils

---
+++ src/utils/color/color.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define COLOR_RED        "\x1b[31m"
+#define COLOR_GREEN      "\x1b[32m"
+#define COLOR_YELLOW     "\x1b[33m"
+#define COLOR_BLUE       "\x1b[34m"
+#define COLOR_MAGENTA    "\x1b[35m"
+#define COLOR_CYAN       "\x1b[36m"
+#define COLOR_WHITE      "\x1b[37m"
+#define COLOR_RESET      "\x1b[0m"
+
+#define BG_COLOR_RED     "\x1b[41m"
+#define BG_COLOR_GREEN   "\x1b[42m"
+#define BG_COLOR_YELLOW  "\x1b[43m"
+#define BG_COLOR_BLUE    "\x1b[44m"
+#define BG_COLOR_MAGENTA "\x1b[45m"
+#define BG_COLOR_CYAN    "\x1b[46m"
+#define BG_COLOR_WHITE   "\x1b[47m"
+
+#define CLEAR_SCREEN     "\e[1;1H\e[2J"
+
+#endif
