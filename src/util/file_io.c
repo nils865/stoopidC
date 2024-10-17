95902b00c8ad81921126f9785e7b552b3f8ee609
Nils <nils.staas0806@gmail.com>
2024-10-17 11:13:45 +0200

    Error when file not found

--- src/util/file_io.c
+++ src/util/file_io.c
@@ -1,5 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 void read_file(char** name) {
-    printf("%s", *name);
+    FILE *file = fopen(*name, "rb");
+
+    if (file == NULL) {
+        printf("File %s not found\n", *name);
+        return;
+    }
+
+    printf("found\n");
+
 }
